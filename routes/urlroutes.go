511f4ab9291d0232276a9469b5b05d45616fae2f
Joseph Martinez <josephsmartinez@gmail.com>
2019-07-15 01:00:34 -0700

    update

--- routes/urlroutes.go
+++ routes/urlroutes.go
@@ -1,14 +1,14 @@
 package routes
 
-func URL1() string{
+func URL1() string {
 	mess := "url1"
 	return mess
 }
-func URL2() string{
+func URL2() string {
 	mess := "url2"
 	return mess
 }
-func URL3() string{
+func URL3() string {
 	mess := "url3"
 	return mess
 }
